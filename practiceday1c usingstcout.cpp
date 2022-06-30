cc997827cc310ab7647cf547eba57a5d230148d4
Sauru6527 <89717054+Sauru6527@users.noreply.github.com>
2022-06-30 14:33:30 +0530

    Add files via upload

---
+++ practiceday1c usingstcout.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+//using namespace std;
+using std::cout;
+using std::cin;
+using std::endl;
+int main()
+{
+	cout<<"ENTER NUMBER ::>>>";
+	int n1 = 0;
+	//std::cin>>n1; 
+	/*std::*/
+	cin>>n1;
+	cout<<"THE ENTERED NUMBER IS => "<<n1;
+	cout<<"USING THE ENDL"<<endl;
+	
+	return 0;
+	
+}
+
+
